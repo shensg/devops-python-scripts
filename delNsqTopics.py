56b0af3a80231d2be0c4f25863e10b85438102b4
shenwxopen <38278523+shenwxopen@users.noreply.github.com>
2022-04-20 15:04:02 +0800

    兼容1.1.0和1.2.0的无用topics和channel的删除

--- delNsqTopics.py
+++ delNsqTopics.py
@@ -2,57 +2,73 @@
 # --*--coding:utf-8--*--
 # Author: Shensg
 
+"""
+This's delete NSQ topic and channel!!!!
+"""
+
+import re
 import requests
 import time
 from requests.auth import HTTPBasicAuth
 
-# 获取nsq所有topic
+# Obtain nsq version
+def getNsqVersion(addr):
+    r = requests.get(addr, auth=HTTPBasicAuth(username, password))
+    # print(r.text)
+    s = re.findall('(\s.*[a-z].+?\sVERSION.*)', r.text)
+    s = str(s)
+    v = re.split(r'[=\'\"]', s)
+    # print(v[3])
+    return v[3]
+
+# Obtain nsq all topic
 def delUnusedTopicAndChannel(addr):
-    f = requests.get(addr, auth=HTTPBasicAuth(username, password))
+    version = getNsqVersion(addr)
+    r = re.split('[.]', version)
+    r = int(r[1])
+    f = requests.get(addr + "/api/topics", auth=HTTPBasicAuth(username, password))
     f = f.json()
-    # print("所有topic")
+    # print("All topic")
     # print(f)
     topicList = f.get("topics", None)
     for topic in topicList:
         time.sleep(0.01)
-        r = requests.get(addr + "/" + topic, auth=HTTPBasicAuth(username, password))
+        r = requests.get(addr + "/api/topics/" + topic, auth=HTTPBasicAuth(username, password))
         # r = requests.request("GET", addr + "/" + "production.live_event_push")
-        # print(r.json)
         data = r.json()
         #print(data)
         l = data.get("nodes", None)[0]
-        print(l.get("channels", None))
         if len(l.get("channels", None)):
-            # 删除无用的channel
+            # Delete unused channel
             res = l.get("channels", None)
-            for i in res:
-                """
-                这是删除nsq：v1.2.0 的topic和channel
-                """
-                if i.get("client_count") == 0:
-                    print("删除无用channel", i.get("topic_name", None), i.get("channel_name", None))
-                    r1 = requests.delete(addr + "/" + i.get("topic_name", None) + "/" + i.get("channel_name", None), auth=HTTPBasicAuth(username, password))
-                    print(r1)
-                    time.sleep(0.1)
-                """
-                这是删除nsq：v1.1.0 的topic和channel
-                """
-                if not i.get("clients"):
-                    print("删除无用channel", i.get("topic_name", None), i.get("channel_name", None))
-                    r1 = requests.delete(addr + "/" + i.get("topic_name", None) + "/" + i.get("channel_name", None), auth=HTTPBasicAuth(username, password))
-                    print(r1)
-                    time.sleep(0.1)
+            if r == 1:
+                print("NSQ greater than or equal to 1.1.0, Is the current version：", version)
+                for i in res:
+                    if not i.get("clients"):
+                        print("Delete unused channel", i.get("topic_name", None), i.get("channel_name", None))
+                        r1 = requests.delete(addr + "/api/topics/" + i.get("topic_name", None) + "/" + i.get("channel_name", None), auth=HTTPBasicAuth(username, password))
+                        print(r1)
+                        time.sleep(0.1)
+            elif r == 2:
+                print("NSQ version greater than or equal to, Is the current version：", version)
+                for i in res:
+                    if i.get("client_count") == 0:
+                        print("delete unused channel", i.get("topic_name", None), i.get("channel_name", None))
+                        r1 = requests.delete(addr + "/api/topics/" + i.get("topic_name", None) + "/" + i.get("channel_name", None), auth=HTTPBasicAuth(username, password))
+                        print(r1)
+                        time.sleep(0.1)
         else:
-            # 删除无用的topic
-            print("删除无用topic", l.get("topic_name", None))
-            r1 = requests.delete(addr + "/" + l.get("topic_name", None), auth=HTTPBasicAuth(username, password))
+            # Delete unused topic
+            print("Delete unused topic", l.get("topic_name", None))
+            r1 = requests.delete(addr + "/api/topics/" + l.get("topic_name", None), auth=HTTPBasicAuth(username, password))
             print(r1)
             time.sleep(0.1)
 
 if __name__ == "__main__":
+    # NSQ username password authentication
     username = ""
     password = ""
-    addr = "https://<doamin.com>/api/topics"
+    # The corresponding NSQ address needs to be replaced. Please do not add it after the address "/"
+    addr = "https://<doamin>"
     delUnusedTopicAndChannel(addr)
     delUnusedTopicAndChannel(addr)
-
